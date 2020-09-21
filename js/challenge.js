c80c60d749f888115de4aecb11a5076f1fec861a
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-20 19:14:42 -0700

    Done.

---
+++ js/challenge.js
@@ -0,0 +1,93 @@
+// As a user, I should see the timer increment every second once the page has loaded.
+
+// ``const minus = document.getElementById("minus");
+// const plus = document.getElementById("plus");
+// let counter = document.getElementById("counter");
+// let count = 0;
+// let intervalID = window.setInterval(function () {
+//   return counter + count++;
+// }, 50);
+
+// // function myCallback() {
+// //   // Your code here
+// //   // Parameters are purely optional.
+// //   count++;
+// //   console.log(a);
+// //   console.log(b);
+// // }
+
+// // setInterval(function () {
+// //   d = new Date();
+// //   document.getElementById("h").innerHTML = d.getHours();
+// //   document.getElementById("m").innerHTML = d.getMinutes();
+// //   document.getElementById("s").innerHTML = d.getSeconds();
+// // }, 500);``
+
+function _toConsumableArray(a) {
+  if (Array.isArray(a)) {
+    for (var b = 0, c = Array(a.length); b < a.length; b++) c[b] = a[b];
+    return c;
+  }
+  return Array.from(a);
+}
+var playing = !0,
+  timer = function () {
+    return setInterval(function () {
+      var a = document.getElementById("counter"),
+        b = parseInt(a.innerText);
+      a.innerText = b + 1;
+    }, 1e3);
+  },
+  interval = timer(),
+  minus = document.getElementById("minus"),
+  plus = document.getElementById("plus"),
+  heart = document.getElementById("heart"),
+  pause = document.getElementById("pause"),
+  commentForm = document.getElementsByTagName("form")[0];
+minus.addEventListener("click", function () {
+  var a = document.getElementById("counter"),
+    b = parseInt(a.innerText);
+  a.innerText = b - 1;
+}),
+  plus.addEventListener("click", function () {
+    var a = document.getElementById("counter"),
+      b = parseInt(a.innerText);
+    a.innerText = b + 1;
+  }),
+  heart.addEventListener("click", function () {
+    var a = document.getElementById("counter"),
+      b = parseInt(a.innerText),
+      c = document.querySelector(".likes"),
+      d = void 0;
+    if (
+      []
+        .concat(_toConsumableArray(c.children))
+        .map(function (a) {
+          return parseInt(a.dataset.num);
+        })
+        .includes(b)
+    ) {
+      d = document.querySelector('[data-num="' + b + '"]');
+      var e = parseInt(d.children[0].innerText);
+      d.innerHTML = b + " has been liked <span>" + (e + 1) + "</span> times";
+    } else (d = document.createElement("li")).setAttribute("data-num", b), (d.innerHTML = b + " has been liked <span>1</span> time"), c.appendChild(d);
+  }),
+  pause.addEventListener("click", function () {
+    playing
+      ? ((playing = !1), clearInterval(interval), (this.innerText = "resume"))
+      : ((playing = !0), (interval = timer()), (this.innerText = "pause")),
+      []
+        .concat(_toConsumableArray(document.getElementsByTagName("button")))
+        .forEach(function (a) {
+          "pause" !== a.id && (a.disabled = !playing);
+        });
+  }),
+  commentForm.addEventListener("submit", function (a) {
+    a.preventDefault();
+    var b = this.children[0],
+      c = b.value;
+    b.value = "";
+    var d = document.querySelector(".comments"),
+      e = document.createElement("p");
+    (e.innerText = c), d.appendChild(e);
+  });
